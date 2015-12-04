d0db107c4110d90d018dfc43525e032594071906
gaokai <gaokai@qima-inc.comgit>
2015-12-04 16:20:02 +0800

    improve

--- scripts/coin_generator.js
+++ scripts/coin_generator.js
@@ -1,20 +1,54 @@
+let map = {
+    'line': 'ccccc',
+    'twoLine': 
+`
+ccccc
+ccccc`,
+    'square':
+`
+ccc
+ccc
+ccc`
+};
+
+let keyMap = {
+    c: 'coin'
+}
+
+let coinMatrix = Object.keys(map).map((key) => {
+    return map[key].split('\n');
+});
+
+coinMatrix.forEach(function (matrix, index, array) {
+    array[index] = matrix.filter(function (row, index) {
+        if (index == 0 && row.length == 0) {
+            return false;
+        }
+        return true;
+    });
+});
 
 
 export default class CoinGenerator {
     constructor (game) {
         this.game = game;
         this.rnd = game.rnd;
-        this.queue = [];
     }
 
     create () {
+        let rndIndex = this.rnd.integerInRange(0, coinMatrix.length-1);
+        let matrix = coinMatrix[rndIndex];
         let data = [];
-        for (let i = 0; i < 10; i++) {
-            let x = i * 30;
-            let y = 0;
-            let type = 'coin';
-            data.push({x, y, type});
+        for (let j = 0; j < matrix.length; j++) {
+            let row = matrix[j];
+            for (let i = 0; i < row.length; i++) {
+                let x = i * 30;
+                let y = j * 30;
+                let type = keyMap[row[i]];
+                data.push({x, y, type});
+            }
         }
+        
         return data;
     }
 }
